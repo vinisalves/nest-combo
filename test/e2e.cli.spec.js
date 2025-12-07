b42fa493f88237c936e466c4d1d4a7ae6981eeb9
vinisalves <soft.vini@gmail.com>
2025-12-07 16:49:04 +0100

    feat: add api generation
    add library generation
    
    fix: tests

--- test/e2e.cli.spec.js
+++ test/e2e.cli.spec.js
@@ -3,97 +3,42 @@ const path = require('path');
 const fs = require('fs');
 
 describe('CLI End-to-End Tests', () => {
+  const tmpDir = path.resolve('./tmp');
   const projectName = 'test-project';
+  const projectNameYaml = 'test-project-yaml';
 
   beforeAll(() => {
     execSync('npm link', { cwd: process.cwd() });
 
-    if (fs.existsSync(projectName)) {
-      execSync(`rm -rf ${projectName}`);
+    if (!fs.existsSync(tmpDir)) {
+      fs.mkdirSync(tmpDir);
     }
+
+    const projectDir = path.join(tmpDir, projectName);
+    const projectYamlDir = path.join(tmpDir, projectNameYaml);
+
+    if (fs.existsSync(projectDir)) execSync(`rm -rf ${projectDir}`);
+    if (fs.existsSync(projectYamlDir)) execSync(`rm -rf ${projectYamlDir}`);
   });
 
   afterAll(() => {
-    if (fs.existsSync(projectName)) {
-      execSync(`rm -rf ${projectName}`);
+    if (fs.existsSync(tmpDir)) {
+      execSync(`rm -rf ${tmpDir}`);
     }
   });
 
   it('should create a new project', () => {
-    if (fs.existsSync(projectName)) {
-      execSync(`rm -rf ${projectName}`);
-    }
-    execSync(`nest-combo ${projectName} -new -no-vscode`).toString();
-
-    expect(fs.existsSync(projectName)).toBe(true);
-  });
-
-  it('should generate resources (module, controller, service, gateway, interceptor and middleware)', () => {
-    const output = execSync(`nest-combo users -m -c -s -g -itc -mw`, {
-      cwd: projectName,
+    execSync(`nest-combo ${projectName} -new -no-vscode`, {
+      cwd: tmpDir,
     }).toString();
 
-    expect(output).toContain('Generating module for users');
-    expect(output).toContain('Generating controller for users');
-    expect(output).toContain('Generating service for users');
-
-    const moduleFile = path.join(
-      projectName,
-      'src',
-      'users',
-      'users.module.ts',
-    );
-    const controllerFile = path.join(
-      projectName,
-      'src',
-      'users',
-      'users.controller.ts',
-    );
-    const serviceFile = path.join(
-      projectName,
-      'src',
-      'users',
-      'users.service.ts',
-    );
-    const gatewayFile = path.join(
-      projectName,
-      'src',
-      'users',
-      'users.gateway.ts',
-    );
-    const interceptorFile = path.join(
-      projectName,
-      'src',
-      'users',
-      'users.interceptor.ts',
-    );
-    const middlewareFile = path.join(
-      projectName,
-      'src',
-      'users',
-      'users.middleware.ts',
-    );
-
-    expect(fs.existsSync(moduleFile)).toBe(true);
-    expect(fs.existsSync(controllerFile)).toBe(true);
-    expect(fs.existsSync(serviceFile)).toBe(true);
-    expect(fs.existsSync(gatewayFile)).toBe(true);
-    expect(fs.existsSync(interceptorFile)).toBe(true);
-    expect(fs.existsSync(middlewareFile)).toBe(true);
-
-    if (fs.existsSync(projectName)) {
-      execSync(`rm -rf ${projectName}`);
-    }
+    expect(fs.existsSync(path.join(tmpDir, projectName))).toBe(true);
   });
 
   it('should load a full project from a YAML file', () => {
-    if (fs.existsSync(projectName)) {
-      execSync(`rm -rf ${projectName}`);
-    }
-
     const ymlFileContent = `
 nest-combo:
-  project-name: test-project
+  project-name: test-project-yaml
   package-manager: npm 
   dependencies:
     - "@nestjs/config"    
@@ -139,21 +84,56 @@ nest-combo:
         - service
     `;
 
-    const yamlFilePath = path.resolve(`./test/${projectName}.yml`);
+    const yamlFilePath = path.join(tmpDir, `${projectNameYaml}.yml`);
     fs.writeFileSync(yamlFilePath, ymlFileContent);
 
-    const command = `nest-combo -f ${yamlFilePath}`;
-    const output = execSync(command).toString();
+    const output = execSync(`nest-combo -f ${yamlFilePath}`, {
+      cwd: tmpDir,
+    }).toString();
 
-    expect(fs.existsSync(projectName)).toBe(true);
-
-    expect(output).toContain('Generating project');
-    expect(output).toContain('Installing dependencies');
-    expect(output).toContain('Generating module for core');
-    expect(output).toContain('Generating module for core/user');
-    expect(output).toContain('Generating controller for core/user');
-    expect(output).toContain('Generating service for core/user');
+    expect(fs.existsSync(path.join(tmpDir, projectNameYaml))).toBe(true);
 
     fs.unlinkSync(yamlFilePath);
+  });
+
+  it('should generate resources (module, controller, service, gateway, interceptor and middleware)', () => {
+    const projectDir = path.join(tmpDir, projectName);
+
+    const output = execSync(`nest-combo users -m -c -s -g -itc -mw`, {
+      cwd: projectDir,
+    }).toString();
+
+    expect(output).toContain('Generating module for users');
+
+    const usersDir = path.join(projectDir, 'src', 'users');
+
+    const expectedFiles = [
+      'users.module.ts',
+      'users.controller.ts',
+      'users.service.ts',
+      'users.gateway.ts',
+      'users.interceptor.ts',
+      'users.middleware.ts',
+    ];
+
+    expectedFiles.forEach((file) => {
+      expect(fs.existsSync(path.join(usersDir, file))).toBe(true);
+    });
+  });
+
+  it('should generate API resources', () => {
+    const projectDir = path.join(tmpDir, projectName);
+
+    const output = execSync(`nest-combo auth -a`, {
+      cwd: projectDir,
+    }).toString();
+
+    expect(output).toContain('Generating api for auth');
+
+    const authDir = path.join(projectDir, 'src', 'auth');
+
+    ['auth.module.ts', 'auth.controller.ts', 'auth.service.ts'].forEach(
+      (file) => expect(fs.existsSync(path.join(authDir, file))).toBe(true),
+    );
   });
 });
